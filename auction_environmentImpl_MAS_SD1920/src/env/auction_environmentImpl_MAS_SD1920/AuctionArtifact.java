fd87be280da32e0aaa3970efa3fcc43d5e551452
unknown <rei.beshiri@studio.unibo.it>
2020-02-18 17:43:08 +0100

    add auction info

--- auction_environmentImpl_MAS_SD1920/src/env/auction_environmentImpl_MAS_SD1920/AuctionArtifact.java
+++ auction_environmentImpl_MAS_SD1920/src/env/auction_environmentImpl_MAS_SD1920/AuctionArtifact.java
@@ -3,18 +3,31 @@
 package auction_environmentImpl_MAS_SD1920;
 
 import jason.asSyntax.Atom;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 import cartago.*;
 
 public class AuctionArtifact extends Artifact {
 
     String currentWinner = "no_winner";
     String errorMsg = "no_errors";
+    /*
+     * Double -> value of the bid.
+     * String  -> description of the bid.
+     * */
+    HashMap<Double, String> auctionInfo = new HashMap<Double, String>();
     
     public void init()  {
         // observable properties
         defineObsProperty("running",     "no");
         defineObsProperty("best_bid",    Double.MIN_VALUE);
         defineObsProperty("winner",      new Atom(currentWinner)); //Atom -- the most simple literal, is composed by only a functor (no term, no annots)
+        auctionInfo.put(0.0, "iniziated auction for " + getId().toString());
     }
 
     @OPERATION public void start(String task)  {
@@ -35,6 +48,13 @@ public class AuctionArtifact extends Artifact {
 
         getObsProperty("running").updateValue("no");
         getObsProperty("winner").updateValue(new Atom(currentWinner));
+        //lambda not allowed with this java version
+        //auctionInfo.forEach((k,v) -> System.out.println("key: "+k+" value:"+v));
+        List<Double> sortedKeys=new ArrayList<Double>(auctionInfo.keySet());
+        Collections.sort(sortedKeys);
+    	for(int i = 0; i<sortedKeys.size(); i++) {
+    		System.out.println(sortedKeys.get(i) + " " + auctionInfo.get(sortedKeys.get(i)).toString());
+    	}
     }
 
     @OPERATION public void bid(double bidValue) {
@@ -49,6 +69,7 @@ public class AuctionArtifact extends Artifact {
             currentWinner = getCurrentOpAgentId().getAgentName(); // the name of the agent doing this operation
         }
         System.out.println("Received bid "+bidValue+" from "+getCurrentOpAgentId().getAgentName()+" for "+getObsProperty("task").stringValue());
+        auctionInfo.put(bidValue, "bid from "+getCurrentOpAgentId().getAgentName() + " for " + getId().toString());
     }
     
     @OPERATION public void rebid(double bidValue) {
@@ -60,8 +81,9 @@ public class AuctionArtifact extends Artifact {
         ObsProperty opCurrentValue  = getObsProperty("best_bid");
         if (bidValue > opCurrentValue.doubleValue() && currentWinner != getCurrentOpAgentId().getAgentName()) {  // the bid is better than the previous
             opCurrentValue.updateValue(bidValue);
-            currentWinner = getCurrentOpAgentId().getAgentName(); // the name of the agent doing this operation
-            System.out.println("Received rebid "+bidValue+" from "+getCurrentOpAgentId().getAgentName()+" for "+getObsProperty("task").stringValue());            
+            currentWinner = getCurrentOpAgentId().getAgentName(); // the name of the agent doing this operation            
         }
+        System.out.println("Received rebid "+bidValue+" from "+getCurrentOpAgentId().getAgentName()+" for "+getObsProperty("task").stringValue());
+        auctionInfo.put(bidValue, "bid from "+getCurrentOpAgentId().getAgentName()+ " for " + getId().toString());
     }
 }
